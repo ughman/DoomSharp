59445536013ef57cce65994934194ab63c46b872
chekwob <chekwob@yahoo.com>
2013-12-26 23:25:13 +0000

    Replace w_wad code with stubs

---
+++ Doom/w_wad.cpp
@@ -0,0 +1,43 @@
+extern "C" void W_Reload()
+{
+}
+
+extern "C" void W_InitMultipleFiles(char **filenames)
+{
+	// TODO
+}
+
+extern "C" int W_CheckNumForName(char *name)
+{
+	// TODO
+	return 0;
+}
+
+extern "C" int W_GetNumForName(char *name)
+{
+	// TODO
+	return 0;
+}
+
+extern "C" int W_LumpLength(int lump)
+{
+	// TODO
+	return 0;
+}
+
+extern "C" void W_ReadLump(int lump,void *dest)
+{
+	// TODO
+}
+
+extern "C" void *W_CacheLumpNum(int lump,int tag)
+{
+	// TODO
+	return 0;
+}
+
+extern "C" void *W_CacheLumpName(char *name,int tag)
+{
+	// TODO
+	return 0;
+}
