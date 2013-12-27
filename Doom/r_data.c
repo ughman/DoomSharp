ab4674d8be1bb3c7afcdb47d035c27b0c5752efc
chekwob <chekwob@yahoo.com>
2013-12-27 01:29:57 +0000

    Fix direct lumpinfo size access

--- Doom/r_data.c
+++ Doom/r_data.c
@@ -776,7 +776,7 @@ void R_PrecacheLevel (void)
 	if (flatpresent[i])
 	{
 	    lump = firstflat + i;
-	    flatmemory += lumpinfo[lump].size;
+	    flatmemory += W_LumpLength(lump);
 	    W_CacheLumpNum(lump, PU_CACHE);
 	}
     }
@@ -811,7 +811,7 @@ void R_PrecacheLevel (void)
 	for (j=0 ; j<texture->patchcount ; j++)
 	{
 	    lump = texture->patches[j].patch;
-	    texturememory += lumpinfo[lump].size;
+	    texturememory += W_LumpLength(lump);
 	    W_CacheLumpNum(lump , PU_CACHE);
 	}
     }
@@ -838,7 +838,7 @@ void R_PrecacheLevel (void)
 	    for (k=0 ; k<8 ; k++)
 	    {
 		lump = firstspritelump + sf->lump[k];
-		spritememory += lumpinfo[lump].size;
+		spritememory += W_LumpLength(lump);
 		W_CacheLumpNum(lump , PU_CACHE);
 	    }
 	}
