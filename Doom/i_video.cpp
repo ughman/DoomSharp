15dade2b034cee1e70f74c56e82e13e1a248567f
chekwob <chekwob@yahoo.com>
2014-01-02 18:07:32 +0000

    Implement keyboard input

--- Doom/i_video.cpp
+++ Doom/i_video.cpp
@@ -1,15 +1,22 @@
 using namespace System;
+using namespace System::Threading;
+using namespace System::Collections::Generic;
+using namespace OpenTK;
+using namespace OpenTK::Input;
 using namespace DoomSharp;
 
 extern "C"
 {
 #include "i_video.h"
 #include "v_video.h"
+#include "d_event.h"
+#include "d_main.h"
 }
 
 #include <vcclr.h>
 
 gcroot<MainWindow^> window;
+gcroot<Queue<IntPtr>^> eventqueue;
 
 extern "C" void I_ShutdownGraphics()
 {
@@ -21,9 +28,27 @@ extern "C" void I_StartFrame()
 	// TODO
 }
 
+void I_StartTic2()
+{
+	Monitor::Enter(eventqueue);
+	try
+	{
+		while (eventqueue->Count)
+		{
+			event_t *ev = (event_t *)(void *)eventqueue->Dequeue();
+			D_PostEvent(ev);
+			delete ev;
+		}
+	}
+	finally
+	{
+		Monitor::Exit(eventqueue);
+	}
+}
+
 extern "C" void I_StartTic()
 {
-	// TODO
+	I_StartTic2();
 }
 
 extern "C" void I_UpdateNoBlit()
@@ -46,7 +71,119 @@ extern "C" void I_SetPalette(byte *palette)
 	window->SubmitPalette((IntPtr)palette);
 }
 
+int TranslateKey(Key key)
+{
+	switch (key)
+	{
+	case Key::Right:
+		return KEY_RIGHTARROW;
+	case Key::Left:
+		return KEY_LEFTARROW;
+	case Key::Up:
+		return KEY_UPARROW;
+	case Key::Down:
+		return KEY_DOWNARROW;
+	case Key::Escape:
+		return KEY_ESCAPE;
+	case Key::Enter:
+		return KEY_ENTER;
+	case Key::Tab:
+		return KEY_TAB;
+	case Key::F1:
+		return KEY_F1;
+	case Key::F2:
+		return KEY_F2;
+	case Key::F3:
+		return KEY_F3;
+	case Key::F4:
+		return KEY_F4;
+	case Key::F5:
+		return KEY_F5;
+	case Key::F6:
+		return KEY_F6;
+	case Key::F7:
+		return KEY_F7;
+	case Key::F8:
+		return KEY_F8;
+	case Key::F9:
+		return KEY_F9;
+	case Key::F10:
+		return KEY_F10;
+	case Key::F11:
+		return KEY_F11;
+	case Key::F12:
+		return KEY_F12;
+	case Key::BackSpace:
+		return KEY_BACKSPACE;
+	case Key::Pause:
+		return KEY_PAUSE;
+	case Key::Plus:
+		return KEY_EQUALS;
+	case Key::Minus:
+		return KEY_MINUS;
+	case Key::ShiftLeft:
+	case Key::ShiftRight:
+		return KEY_RSHIFT;
+	case Key::ControlLeft:
+	case Key::ControlRight:
+		return KEY_RCTRL;
+	case Key::AltLeft:
+	case Key::AltRight:
+		return KEY_RALT;
+	case Key::Space:
+		return ' ';
+	case Key::Comma:
+		return ',';
+	case Key::Period:
+		return '.';
+	}
+	if (key >= Key::A && key <= Key::Z)
+	{
+		return (int)(key - Key::A) + 'a';
+	}
+	if (key >= Key::Number0 && key <= Key::Number9)
+	{
+		return (int)(key - Key::Number0) + '0';
+	}
+	return 0;
+}
+
+void HandleKeyUp(Object^ sender,KeyboardKeyEventArgs^ e)
+{
+	Monitor::Enter(eventqueue);
+	try
+	{
+		event_t *ev = new event_t;
+		ev->type = ev_keyup;
+		ev->data1 = TranslateKey(e->Key);
+		eventqueue->Enqueue((IntPtr)ev);
+	}
+	finally
+	{
+		Monitor::Exit(eventqueue);
+	}
+}
+
+void HandleKeyDown(Object^ sender,KeyboardKeyEventArgs^ e)
+{
+	Monitor::Enter(eventqueue);
+	try
+	{
+		event_t *ev = new event_t;
+		ev->type = ev_keydown;
+		ev->data1 = TranslateKey(e->Key);
+		eventqueue->Enqueue((IntPtr)ev);
+	}
+	finally
+	{
+		Monitor::Exit(eventqueue);
+	}
+}
+
 extern "C" void I_InitGraphics()
 {
+	eventqueue = gcnew Queue<IntPtr>();
 	window = MainWindow::RunAsync();
+	window->Keyboard->KeyUp += gcnew EventHandler<KeyboardKeyEventArgs^>(HandleKeyUp);
+	window->Keyboard->KeyDown += gcnew EventHandler<KeyboardKeyEventArgs^>(HandleKeyDown);
 }
