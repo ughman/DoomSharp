e6373a6d473d7312a1cd5ea8b458d1556e058a6d
chekwob <chekwob@yahoo.com>
2014-02-02 05:25:02 +0000

    Track F_ and FF_ marked flats in B_InitLumps

--- Doom/b_lumps.cpp
+++ Doom/b_lumps.cpp
@@ -8,15 +8,22 @@ extern gcroot<MultiArchive^> archives;
 
 gcroot<List<int>^> spritelumps;
 int spritelumpiterator;
+gcroot<List<int>^> flatlumps;
 
 void B_InitLumps2()
 {
 	spritelumps = gcnew List<int>();
+	flatlumps = gcnew List<int>();
 	String^ name_s_start = gcnew String("S_START");
 	String^ name_s_end = gcnew String("S_END");
 	String^ name_ss_start = gcnew String("SS_START");
 	String^ name_ss_end = gcnew String("SS_END");
+	String^ name_f_start = gcnew String("F_START");
+	String^ name_f_end = gcnew String("F_END");
+	String^ name_ff_start = gcnew String("FF_START");
+	String^ name_ff_end = gcnew String("FF_END");
 	bool inspritezone = false;
+	bool inflatzone = false;
 	for (int i = 0;i < archives->LumpCount;i++)
 	{
 		ILump^ lump = archives->default[i];
@@ -31,6 +38,18 @@ void B_InitLumps2()
 		else if (inspritezone)
 		{
 			spritelumps->Add(i);
+		}
+		if (lump->Name == name_f_start || lump->Name == name_ff_start)
+		{
+			inflatzone = true;
+		}
+		else if (lump->Name == name_f_end || lump->Name == name_ff_end)
+		{
+			inflatzone = false;
+		}
+		else if (inflatzone)
+		{
+			flatlumps->Add(i);
 		}
 	}
 }
