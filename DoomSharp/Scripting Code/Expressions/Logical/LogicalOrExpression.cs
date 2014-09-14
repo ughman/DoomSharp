693b25c5af44a098f5979344e84172ee1f8e6149
chekwob <chekwob@yahoo.com>
2014-09-14 23:25:52 +0000

    Add short-circuit evaluation for scripting

--- DoomSharp/Scripting Code/Expressions/Logical/LogicalOrExpression.cs
+++ DoomSharp/Scripting Code/Expressions/Logical/LogicalOrExpression.cs
@@ -12,13 +12,17 @@ namespace DoomSharp
 
         public override Type Evaluate(CodeContext c)
         {
+            Label shortcircuit = c.IL.DefineLabel();
             Type lefttype = Left.Evaluate(c);
+            c.IL.Emit(OpCodes.Dup);
+            c.IL.Emit(OpCodes.Brtrue,shortcircuit);
             Type righttype = Right.Evaluate(c);
             if (lefttype != typeof(bool) || righttype != typeof(bool))
             {
                 throw new ApplicationException();
             }
             c.IL.Emit(OpCodes.Or);
+            c.IL.MarkLabel(shortcircuit);
             return typeof(bool);
         }
     }
