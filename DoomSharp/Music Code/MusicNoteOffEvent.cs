4ab65bd9cb2d2a9eefa713d523dbf8fdeeefa624
chekwob <chekwob@yahoo.com>
2014-10-04 23:20:23 +0000

    Correct a field name in MusicNoteOffEvent

--- DoomSharp/Music Code/MusicNoteOffEvent.cs
+++ DoomSharp/Music Code/MusicNoteOffEvent.cs
@@ -5,23 +5,23 @@ namespace DoomSharp
 {
     public sealed class MusicNoteOffEvent : MusicEvent
     {
-        private int note;
+        private int volume;
 
-        public MusicNoteOffEvent(int note,int channel,int delay) : base(channel,delay)
+        public MusicNoteOffEvent(int volume,int channel,int delay) : base(channel,delay)
         {
-            if (note < 0 || note >= 128)
-                throw new ArgumentOutOfRangeException("note");
-            this.note = note;
+            if (volume < 0 || volume >= 128)
+                throw new ArgumentOutOfRangeException("volume");
+            this.volume = volume;
         }
 
-        public int Note
+        public int Volume
         {
-            get { return note; }
+            get { return volume; }
         }
 
         public override void Play(Synthesizer synth)
         {
-            synth.NoteOff(MIDIChannel,(short)note);
+            synth.NoteOff(MIDIChannel,(short)volume);
         }
     }
 }
